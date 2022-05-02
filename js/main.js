0cb11f2d209a5d7d1cd2d1742dd9f7db4273fc8e
kouthk <alex_spg@outlook.com>
2022-05-02 17:56:39 -0400

    validação do CNPJ

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ fetch("../lista_cnae.json")
   })
   .then((data) => {
     // Work with JSON data here
-    new SlimSelect({
+    var select1 = new SlimSelect({
       select: "#select_CNAEs_atividadeEconomica",
       placeholder: "Selecione a atividade principal",
       searchPlaceholder: "Buscar:",
@@ -138,42 +138,101 @@ document
   .getElementById("id_cnpj_cpf_identificacao")
   .addEventListener("focusout", function () {
     let cnpj = document.getElementById("id_cnpj_cpf_identificacao").value;
-    cnpj = cnpj.replace(/[^\d]+/g, "");
+    if (!is_cnpj(cnpj)) {
+      alert(" O CNPJ:"+ cnpj+" é inválido. Limparemos o campo para que vocÊ digite novamente.");
+      document.getElementById("id_cnpj_cpf_identificacao").value = "";
+    } else {
+      cnpj = cnpj.replace(/[^\d]+/g, "");
+      $.ajax({
+        type: "GET",
+        dataType: "jsonp",
+        url: `https://www.receitaws.com.br/v1/cnpj/${cnpj}`,
+        success: function (data) {
+          console.log(data);
 
-    $.ajax({
-      type: "GET",
-      dataType: "jsonp",
-      url: `https://www.receitaws.com.br/v1/cnpj/${cnpj}`,
-      success: function (data) {
-        console.log(data);
-
-        document.getElementById("id_nameFantasia_identificacao").value =
-          data.fantasia;
-        document.getElementById("id_nameRazaoSocial_identificacao").value =
-          data.nome;
-        document.getElementById("id_dataAbertura_identificacao").value =
-          data.abertura;
-        document.getElementById("id_email_identificacao").value = data.email;
-        //Localização das atividades
-        document.getElementById("id_cep_localizacao_das_atividades").value =
-          data.cep.replace(/[^\d]+/g, "");
-        document.getElementById(
-          "id_logradouro_localizacao_das_atividades"
-        ).value = data.logradouro;
-        document.getElementById("id_numero_localizacao_das_atividades").value =
-          data.numero;
-        document.getElementById(
-          "id_complementar_localizacao_das_atividades"
-        ).value = data.complemento;
-        document.getElementById("id_bairro_localizacao_das_atividades").value =
-          data.bairro;
-        document.getElementById("id_cidade_localizacao_das_atividades").value =
-          data.municipio;
-        document.getElementById("id_uf_localizacao_das_atividades").value =
-          data.uf;
-        document.getElementById(
-          "id_telefone_localizacao_das_atividades"
-        ).value = data.telefone;
-      },
-    });
+          document.getElementById("id_nameFantasia_identificacao").value =
+            data.fantasia;
+          document.getElementById("id_nameRazaoSocial_identificacao").value =
+            data.nome;
+          document.getElementById("id_dataAbertura_identificacao").value =
+            data.abertura;
+          document.getElementById("id_email_identificacao").value = data.email;
+          //Localização das atividades
+          document.getElementById("id_cep_localizacao_das_atividades").value =
+            data.cep.replace(/[^\d]+/g, "");
+          document.getElementById(
+            "id_logradouro_localizacao_das_atividades"
+          ).value = data.logradouro;
+          document.getElementById(
+            "id_numero_localizacao_das_atividades"
+          ).value = data.numero;
+          document.getElementById(
+            "id_complementar_localizacao_das_atividades"
+          ).value = data.complemento;
+          document.getElementById(
+            "id_bairro_localizacao_das_atividades"
+          ).value = data.bairro;
+          document.getElementById(
+            "id_cidade_localizacao_das_atividades"
+          ).value = data.municipio;
+          document.getElementById("id_uf_localizacao_das_atividades").value =
+            data.uf;
+          document.getElementById(
+            "id_telefone_localizacao_das_atividades"
+          ).value = data.telefone;
+        },
+      });
+    }
   });
+
+function is_cnpj(cnpj) {
+  cnpj = cnpj.replace(/[^\d]+/g,'');
+
+  if(cnpj == '') return false;
+
+  if (cnpj.length != 14)
+      return false;
+
+  // LINHA 10 - Elimina CNPJs invalidos conhecidos
+  if (cnpj == "00000000000000" || 
+      cnpj == "11111111111111" || 
+      cnpj == "22222222222222" || 
+      cnpj == "33333333333333" || 
+      cnpj == "44444444444444" || 
+      cnpj == "55555555555555" || 
+      cnpj == "66666666666666" || 
+      cnpj == "77777777777777" || 
+      cnpj == "88888888888888" || 
+      cnpj == "99999999999999")
+      return false; // LINHA 21
+
+  // Valida DVs LINHA 23 -
+  tamanho = cnpj.length - 2
+  numeros = cnpj.substring(0,tamanho);
+  digitos = cnpj.substring(tamanho);
+  soma = 0;
+  pos = tamanho - 7;
+  for (i = tamanho; i >= 1; i--) {
+    soma += numeros.charAt(tamanho - i) * pos--;
+    if (pos < 2)
+          pos = 9;
+  }
+  resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+  if (resultado != digitos.charAt(0))
+      return false;
+
+  tamanho = tamanho + 1;
+  numeros = cnpj.substring(0,tamanho);
+  soma = 0;
+  pos = tamanho - 7;
+  for (i = tamanho; i >= 1; i--) {
+    soma += numeros.charAt(tamanho - i) * pos--;
+    if (pos < 2)
+          pos = 9;
+  }
+  resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+  if (resultado != digitos.charAt(1))
+        return false; // LINHA 49
+
+  return true; // LINHA 51
+}
