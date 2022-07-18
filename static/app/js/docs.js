22a9d97d1c3c9a0919c7719dc1f0b33827f881e1
kouthk <alex_spg@outlook.com>
2022-07-18 09:47:18 -0400

    Profissional Liberal - Forms/docs

--- static/app/js/docs.js
+++ static/app/js/docs.js
@@ -1,33 +1,33 @@
-let getData = localStorage.getItem("dataJson");
+let getData = localStorage.getItem("dataJson_ProfissionalLiberal");
 let data = JSON.parse(getData);
+console.log(data)
 
+
+//Profissional Liberal
 document.getElementById("tipoDeRequisicao").innerHTML =
   data.tipoDeRequisicao.tipoRequisiçaoDoc;
-document.getElementById("tipoContribuinte").innerHTML =
-  data.identificacao.tipoContribuinte;
-document.getElementById("id_cnpj_cpf_identificacao").innerHTML =
-  data.identificacao.id_cnpj_cpf_identificacao;
-document.getElementById("id_dataAbertura_identificacao").innerHTML =
-  data.identificacao.id_dataAbertura_identificacao;
-document.getElementById("id_nameRazaoSocial_identificacao").innerHTML =
-  data.identificacao.id_nameRazaoSocial_identificacao;
-document.getElementById("id_nomeFantasia_identificacao").innerHTML =
-  data.identificacao.id_nameFantasia_identificacao;
-document.getElementById("id_email_identificacao").innerHTML =
-  data.identificacao.id_email_identificacao;
-document.getElementById("id_obsRelevante_identificacao").innerHTML =
-  data.identificacao.id_obsRelevante_identificacao;
+document.getElementById("id_cpf_ou_cnpj_proprietario").innerHTML =
+  data.ProfissionalLiberal.id_cpf_ou_cnpj_proprietario;
+document.getElementById("id_nomeRazaoSocial_proprietario").innerHTML =
+  data.ProfissionalLiberal.id_nomeRazaoSocial_proprietario;
+document.getElementById("id_nome_mae_proprietario").innerHTML =
+  data.ProfissionalLiberal.id_nome_mae_proprietario;
+document.getElementById("id_email_proprietario").innerHTML =
+  data.ProfissionalLiberal.id_email_proprietario;
+document.getElementById("id_telefone_proprietario").innerHTML =
+  data.ProfissionalLiberal.id_telefone_proprietario;
+document.getElementById("id_telefone_celular_proprietario").innerHTML =
+  data.ProfissionalLiberal.id_telefone_celular_proprietario
+document.getElementById("id_data_nascimento_proprietario").innerHTML =
+  data.ProfissionalLiberal.id_data_nascimento_proprietario;
+document.getElementById("select_estado_civil_proprietario").innerHTML =
+  data.ProfissionalLiberal.select_estado_civil_proprietario;
 // Atividade Econômica
-
-document.getElementById("select_atividadeEconomica").innerHTML =
-  data.atvEconomica.select_atividadeEconomica;
 document.getElementById("select_CNAEs_atividadeEconomica").innerHTML =
   data.atvEconomica.select_CNAEs_atividadeEconomica;
-document.getElementById(
-  "select_CNAEs_secundario_atividadeEconomica"
-).innerHTML = data.atvEconomica.select_CNAEs_secundario_atividadeEconomica;
 
 //Endereço: Localização das atividades
+document.getElementById("id_inscricao_municipal").innerHTML = data.localizacao_das_atividades.id_inscricao_municipal;
 document.getElementById("id_logradouro_localizacao_das_atividades").innerHTML =
   data.localizacao_das_atividades.id_logradouro_localizacao_das_atividades;
 document.getElementById("id_numero_localizacao_das_atividades").innerHTML =
@@ -40,45 +40,25 @@ document.getElementById(
   "id_complementar_localizacao_das_atividades"
 ).innerHTML =
   data.localizacao_das_atividades.id_complementar_localizacao_das_atividades;
-document.getElementById("id_telefone_localizacao_das_atividades").innerHTML =
-  data.localizacao_das_atividades.id_telefone_localizacao_das_atividades;
 document.getElementById("id_cidade_localizacao_das_atividades").innerHTML =
   data.localizacao_das_atividades.id_cidade_localizacao_das_atividades;
 document.getElementById("id_uf_localizacao_das_atividades").innerHTML =
   data.localizacao_das_atividades.id_uf_localizacao_das_atividades;
 
-//Proprietario
-document.getElementById("id_nomeRazaoSocial_proprietario").innerHTML =
-  data.proprietario.id_nomeRazaoSocial_proprietario;
-document.getElementById("id_nome_mae_proprietario").innerHTML =
-  data.proprietario.id_nome_mae_proprietario;
-document.getElementById("id_data_nascimento_proprietario").innerHTML =
-  data.proprietario.id_data_nascimento_proprietario;
-document.getElementById("select_estado_civil_proprietario").innerHTML =
-  data.proprietario.select_estado_civil_proprietario;
-document.getElementById("id_cpf_ou_cnpj_proprietario").innerHTML =
-  data.proprietario.id_cpf_ou_cnpj_proprietario;
-document.getElementById("id_logradouro_proprietario").innerHTML =
-  data.proprietario.id_logradouro_proprietario;
-document.getElementById("id_num_da_rua_proprietario").innerHTML =
-  data.proprietario.id_num_da_rua_proprietario;
-document.getElementById("id_bairro_proprietario").innerHTML =
-  data.proprietario.id_bairro_proprietario;
-document.getElementById("id_complemento_proprietario").innerHTML =
-  data.proprietario.id_complemento_proprietario;
-document.getElementById("id_cidade_proprietario").innerHTML =
-  data.proprietario.id_cidade_proprietario;
-document.getElementById("id_uf_proprietario").innerHTML =
-  data.proprietario.id_uf_proprietario;
-document.getElementById("id_cep_proprietario").innerHTML =
-  data.proprietario.id_cep_proprietario;
-document.getElementById("id_email_proprietario").innerHTML =
-  data.proprietario.id_email_proprietario;
-document.getElementById("id_telefone_proprietario").innerHTML =
-  data.proprietario.id_telefone_proprietario;
-document.getElementById("id_telefone_celular_proprietario").innerHTML =
-  data.proprietario.id_telefone_celular_proprietario;
-
-document
-  .getElementById("id_obsRelevante_identificacao")
-  .classList.add("text-justify");
+if (data.localizacao_das_atividades.endrecoIgual_localizacao_das_atividades != "Sim") {
+  document.getElementById("end_correspondencia").classList.remove("d-none");
+  document.getElementById("id_Logradouro_endereco_correspondencia").innerHTML =
+    data.endrecoCorrespondencia.id_Logradouro_endereco_correspondencia;
+  document.getElementById("id_numero_endereco_correspondencia").innerHTML =
+    data.endrecoCorrespondencia.id_numero_endereco_correspondencia;
+  document.getElementById("id_bairro_endereco_correspondencia").innerHTML =
+    data.endrecoCorrespondencia.id_bairro_endereco_correspondencia;
+  document.getElementById("id_cep_endereco_correspondencia").innerHTML =
+    data.endrecoCorrespondencia.id_cep_endereco_correspondencia;
+  document.getElementById("id_complemento_endereco_correspondencia").innerHTML =
+    data.endrecoCorrespondencia.id_complemento_endereco_correspondencia;
+  document.getElementById("id_cidade_endereco_correspondencia").innerHTML =
+    data.endrecoCorrespondencia.id_cidade_endereco_correspondencia;
+  document.getElementById("id_uf_endereco_correspondencia").innerHTML =
+    data.endrecoCorrespondencia.id_uf_endereco_correspondencia;
+}
--- static/app/js/validation.js
+++ static/app/js/validation.js
@@ -4,42 +4,17 @@ function saveLocalStorage() {
   ).value;
   console.log(e);
   var a = {
-    tipoDeRequisicao: { tipoRequisiçaoDoc: e },
-    identificacao: {
-      tipoContribuinte: document.querySelector(
-        'input[name="tipoContribuinte"]:checked'
-      ).value,
-      id_cnpj_cpf_identificacao: document.getElementById(
-        "id_cnpj_cpf_identificacao"
-      ).value,
-      id_dataAbertura_identificacao: document.getElementById(
-        "id_dataAbertura_identificacao"
-      ).value,
-      id_nameRazaoSocial_identificacao: document.getElementById(
-        "id_nameRazaoSocial_identificacao"
-      ).value,
-      id_nameFantasia_identificacao: document.getElementById(
-        "id_nameFantasia_identificacao"
-      ).value,
-      id_email_identificacao: document.getElementById("id_email_identificacao")
-        .value,
-      id_obsRelevante_identificacao: document.getElementById(
-        "id_obsRelevante_identificacao"
-      ).value,
+    tipoDeRequisicao: {
+      tipoRequisiçaoDoc: e
     },
     atvEconomica: {
-      select_atividadeEconomica: document.getElementById(
-        "select_atividadeEconomica"
-      ).value,
       select_CNAEs_atividadeEconomica: document.getElementById(
         "select_CNAEs_atividadeEconomica"
       ).value,
-      select_CNAEs_secundario_atividadeEconomica: [
-        ...document.getElementById("select_CNAEs_secundario_atividadeEconomica")
-          .selectedOptions,
-      ].map((e) => e.value),
     },
     localizacao_das_atividades: {
+      id_inscricao_municipal: document.getElementById(
+        "id_inscricao_municipal").value,
       id_logradouro_localizacao_das_atividades: document.getElementById(
         "id_logradouro_localizacao_das_atividades"
       ).value,
@@ -55,9 +30,6 @@ function saveLocalStorage() {
       id_cep_localizacao_das_atividades: document.getElementById(
         "id_cep_localizacao_das_atividades"
       ).value,
-      id_telefone_localizacao_das_atividades: document.getElementById(
-        "id_telefone_localizacao_das_atividades"
-      ).value,
       id_cidade_localizacao_das_atividades: document.getElementById(
         "id_cidade_localizacao_das_atividades"
       ).value,
@@ -68,30 +40,7 @@ function saveLocalStorage() {
         'input[name="endrecoIgual_localizacao_das_atividades"]:checked'
       ).value,
     },
-    endrecoCorrespondencia: {
-      id_Logradouro_endereco_correspondencia: document.getElementById(
-        "id_Logradouro_endereco_correspondencia"
-      ).value,
-      id_numero_endereco_correspondencia: document.getElementById(
-        "id_numero_endereco_correspondencia"
-      ).value,
-      id_bairro_endereco_correspondencia: document.getElementById(
-        "id_bairro_endereco_correspondencia"
-      ).value,
-      id_complemento_endereco_correspondencia: document.getElementById(
-        "id_complemento_endereco_correspondencia"
-      ).value,
-      id_cep_endereco_correspondencia: document.getElementById(
-        "id_cep_endereco_correspondencia"
-      ).value,
-      id_cidade_endereco_correspondencia: document.getElementById(
-        "id_cidade_endereco_correspondencia"
-      ).value,
-      id_uf_endereco_correspondencia: document.getElementById(
-        "id_uf_endereco_correspondencia"
-      ).value,
-    },
-    proprietario: {
+    ProfissionalLiberal: {
       id_nomeRazaoSocial_proprietario: document.getElementById(
         "id_nomeRazaoSocial_proprietario"
       ).value,
@@ -109,32 +58,23 @@ function saveLocalStorage() {
       ).value,
       id_email_proprietario: document.getElementById("id_email_proprietario")
         .value,
-      id_logradouro_proprietario: document.getElementById(
-        "id_logradouro_proprietario"
-      ).value,
-      id_num_da_rua_proprietario: document.getElementById(
-        "id_num_da_rua_proprietario"
-      ).value,
-      id_bairro_proprietario: document.getElementById("id_bairro_proprietario")
-        .value,
-      id_cidade_proprietario: document.getElementById("id_cidade_proprietario")
-        .value,
-      id_uf_proprietario: document.getElementById("id_uf_proprietario").value,
-      id_complemento_proprietario: document.getElementById(
-        "id_complemento_proprietario"
-      ).value,
-      id_cep_proprietario: document.getElementById("id_cep_proprietario").value,
-      id_telefone_proprietario: document.getElementById(
-        "id_telefone_proprietario"
-      ).value,
+      id_telefone_proprietario: document.getElementById("id_telefone_proprietario").value,
       id_telefone_celular_proprietario: document.getElementById(
         "id_telefone_celular_proprietario"
       ).value,
     },
+    endrecoCorrespondencia: {
+      id_Logradouro_endereco_correspondencia: document.getElementById("id_Logradouro_endereco_correspondencia").value,
+      id_numero_endereco_correspondencia: document.getElementById("id_numero_endereco_correspondencia").value,
+      id_bairro_endereco_correspondencia: document.getElementById("id_bairro_endereco_correspondencia").value,
+      id_complemento_endereco_correspondencia: document.getElementById("id_complemento_endereco_correspondencia").value,
+      id_cep_endereco_correspondencia: document.getElementById("id_cep_endereco_correspondencia").value,
+      id_cidade_endereco_correspondencia: document.getElementById("id_cidade_endereco_correspondencia").value,
+      id_uf_endereco_correspondencia: document.getElementById("id_uf_endereco_correspondencia").value,
+    }
   };
-  localStorage.setItem("dataJson", JSON.stringify(a));
-}
-!(function () {
+  localStorage.setItem("dataJson_ProfissionalLiberal", JSON.stringify(a));
+} !(function () {
   "use strict";
   var e = document.querySelectorAll(".needs-validation");
   console.log(e),
@@ -144,11 +84,11 @@ function saveLocalStorage() {
         function (a) {
           console.log(a),
             e.checkValidity() ||
-              (a.preventDefault(),
+            (a.preventDefault(),
               a.stopPropagation(),
               $("input:invalid").filter(":first").focus()),
             e.checkValidity() &&
-              (a.preventDefault(),
+            (a.preventDefault(),
               saveLocalStorage(),
               (location.href = "../../../templates/documento_gerado.html")),
             e.classList.add("was-validated");
@@ -158,3 +98,214 @@ function saveLocalStorage() {
     }),
     console.log("ola");
 })();
+
+let select1 = new SlimSelect({
+  select: "#select_CNAEs_atividadeEconomica",
+  placeholder: "Selecione a atividade principal",
+  searchPlaceholder: "Buscar:",
+  limit: 1,
+});
+function is_cnpj(e) {
+  if ("" == (e = e.replace(/[^\d]+/g, ""))) return !1;
+  if (14 != e.length) return !1;
+  if (
+    "00000000000000" == e ||
+    "11111111111111" == e ||
+    "22222222222222" == e ||
+    "33333333333333" == e ||
+    "44444444444444" == e ||
+    "55555555555555" == e ||
+    "66666666666666" == e ||
+    "77777777777777" == e ||
+    "88888888888888" == e ||
+    "99999999999999" == e
+  )
+    return !1;
+  for (
+    tamanho = e.length - 2,
+    numeros = e.substring(0, tamanho),
+    digitos = e.substring(tamanho),
+    soma = 0,
+    pos = tamanho - 7,
+    i = tamanho;
+    i >= 1;
+    i--
+  )
+    (soma += numeros.charAt(tamanho - i) * pos--), pos < 2 && (pos = 9);
+  if (
+    ((resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11)),
+      resultado != digitos.charAt(0))
+  )
+    return !1;
+  for (
+    tamanho += 1,
+    numeros = e.substring(0, tamanho),
+    soma = 0,
+    pos = tamanho - 7,
+    i = tamanho;
+    i >= 1;
+    i--
+  )
+    (soma += numeros.charAt(tamanho - i) * pos--), pos < 2 && (pos = 9);
+  return (
+    (resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11)),
+    resultado == digitos.charAt(1)
+  );
+}
+function isCPF(e) {
+  if (
+    11 != (e = e.replace(/\D/g, "")).toString().length ||
+    /^(\d)\1{10}$/.test(e)
+  )
+    return !1;
+  var a = !0;
+  return (
+    [9, 10].forEach(function (t) {
+      var o,
+        i = 0;
+      e
+        .split(/(?=)/)
+        .splice(0, t)
+        .forEach(function (e, a) {
+          i += parseInt(e) * (t + 2 - (a + 1));
+        }),
+        (o = (o = i % 11) < 2 ? 0 : 11 - o) != e.substring(t, t + 1) &&
+        (a = !1);
+    }),
+    a
+  );
+}
+fetch("../static/app/json/lista_cnae.json")
+  .then((e) => e.json())
+  .then((e) => {
+    select1.setData(e);
+  })
+  .catch((e) => {
+    console.log(e);
+  }),
+  $("#endereco_igual").change(function () {
+    let e = document.querySelector(
+      'input[name="endrecoIgual_localizacao_das_atividades"]:checked'
+    ).value;
+    localStorage.setItem("endereco_igual", e),
+      "1" === e
+        ? document
+          .getElementById("endereco_correspondencia")
+          .classList.add("d-none")
+        : document
+          .getElementById("endereco_correspondencia")
+          .classList.remove("d-none");
+  }),
+  document
+    .getElementById("id_inscricao_municipal")
+    .addEventListener("focusout", function () {
+      let e = document.getElementById(
+        "id_inscricao_municipal"
+      ).value;
+      fetch(`http://192.168.0.220:3001/imovel/validar/${e}`).then((e) => e.json())
+        .then((e) => {
+          console.log(e)
+            ,
+            (document.getElementById(
+              "id_logradouro_localizacao_das_atividades"
+            ).value = e.logradouro.endereco),
+            (document.getElementById("id_cep_localizacao_das_atividades").value = e.logradouro.cep),
+            (document.getElementById("id_numero_localizacao_das_atividades").value = e.numero),
+            (document.getElementById(
+              "id_bairro_localizacao_das_atividades"
+            ).value = e.logradouro.bairro.nome),
+            (document.getElementById("id_complementar_localizacao_das_atividades").value = e.complemento),
+            (document.getElementById(
+              "id_cidade_localizacao_das_atividades"
+            ).value = e.logradouro.bairro.cidade.nome),
+            (document.getElementById("id_uf_localizacao_das_atividades").value =
+              e.logradouro.bairro.cidade.estado.uf),
+            (document.getElementById(
+              "id_telefone_localizacao_das_atividades"
+            ).value = "66");
+        });
+    }), document
+      .getElementById("id_cep_endereco_correspondencia")
+      .addEventListener("focusout", function () {
+        let e = document.getElementById("id_cep_endereco_correspondencia").value;
+        fetch(`https://viacep.com.br/ws/${e}/json/`)
+          .then((e) => e.json())
+          .then((e) => {
+            console.log(e),
+              (document.getElementById(
+                "id_Logradouro_endereco_correspondencia"
+              ).value = e.logradouro),
+              (document.getElementById(
+                "id_bairro_endereco_correspondencia"
+              ).value = e.bairro),
+              (document.getElementById(
+                "id_cidade_endereco_correspondencia"
+              ).value = e.localidade),
+              (document.getElementById("id_uf_endereco_correspondencia").value =
+                e.uf);
+          });
+      }),
+  document
+    .getElementById("id_cpf_ou_cnpj_proprietario")
+    .addEventListener("focusout", function () {
+      let e = document.getElementById("id_cpf_ou_cnpj_proprietario").value;
+      isCPF(e) ||
+        (alert(
+          " O CPF:" +
+          e +
+          " é inválido. Limparemos o campo para que você digite novamente."
+        ),
+          (document.getElementById("id_cpf_ou_cnpj_proprietario").value = ""));
+    }),
+
+  $("#endereco_igual").change(function () {
+    let optionValue = document.querySelector(
+      'input[name="endrecoIgual_localizacao_das_atividades"]:checked'
+    ).value;
+    localStorage.setItem("endereco_igual", optionValue);
+
+    if (optionValue === "Sim") {
+      document.getElementById("endereco_correspondencia").classList.add("d-none");
+    } else {
+      document.getElementById("endereco_correspondencia").classList.remove("d-none");
+    }
+  });
+
+$("#typeRequestDoc").change(function () {
+  let optionValue = document.querySelector(
+    'input[name="tipoRequisiçaoDoc"]:checked'
+  ).value;
+  if (optionValue === "CADASTRAMENTO") {
+    document.getElementById("cadastroUsoDeSolo").classList.remove("d-none");
+    document.getElementById("alteracaoUsoDeSolo").classList.add("d-none");
+    let op = document.querySelector('input[name="possui_Certidao"]:checked').value
+    disableInputs(!op)
+  } else if (optionValue === "ALTERAÇÃO") {
+    document.getElementById("cadastroUsoDeSolo").classList.add("d-none");
+    document.getElementById("alteracaoUsoDeSolo").classList.remove("d-none");
+    let op = document.querySelector('input[name="alteracao"]:checked').value
+    disableInputs(!op)
+  } else {
+    document.getElementById("cadastroUsoDeSolo").classList.add("d-none");
+    document.getElementById("alteracaoUsoDeSolo").classList.add("d-none");
+    disableInputs(false)
+  }
+});
+
+function disableInputs(status) {
+  document.querySelectorAll(".disabled-input-for-js").forEach(item => {
+    item.disabled = status;
+  })
+}
+
+$("#select_cadastroUsoDeSolo").change(function () {
+  let op = document.querySelector('input[name="possui_Certidao"]:checked').value
+  disableInputs(!op)
+})
+
+
+$("#select_alteracao").change(function () {
+  let op = document.querySelector('input[name="alteracao"]:checked').value
+  disableInputs(!op)
+})
+
